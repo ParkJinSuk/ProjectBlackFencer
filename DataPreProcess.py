60ae26bed01045ec1f406aa9d127a33184d6d1ae
ParkJinSuk <ParkJinSuk@users.noreply.github.com>
2020-01-11 23:19:41 +0900

    200111
    배경제거 함수 구현

--- DataPreProcess.py
+++ DataPreProcess.py
@@ -1,26 +1,24 @@
-###################################################
-# 코드 작성일시 : 2020년 1월 11일
-# 코드 내용 : 특정색깔을 검출하기 위한 함수들을 작성
-###################################################
+"""
+코드 작성일시 : 2020년 1월 11일
+작성자 : Park Jinsuk
+코드 내용 : 특정색깔을 검출하기 위한 함수들을 작성
+"""
 
 import cv2
 
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-
-# 색상 범위
+# 색상 범위 - HSV - 색상(Hue), 채도(Saturation), 명도(Value)
 lower_orange = (8, 50, 50)
 upper_orange = (20, 255, 255)
 
 lower_white = (0, 0, 210)
 upper_white = (255, 255, 255)
 
-def find_line_orange (img):
+
+# 차선의 주황색을 검출하기 위한 함수 #
+def find_line_orange(img):
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # 색상 범위를 제한하여 mask 생성
-    img_mask = cv2.inRange(img_hsv, lower_orange, upper_orange)
     img_mask = cv2.inRange(img_hsv, lower_orange, upper_orange)
 
     # 원본 이미지를 가지고 Object 추출 이미지로 생성
@@ -28,14 +26,28 @@ def find_line_orange (img):
 
     return img_result
 
-def find_line_white (img):
+
+# 차선의 하얀색을 검출하기 위한 함수 #
+def find_line_white(img):
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # 색상 범위를 제한하여 mask 생성
     img_mask = cv2.inRange(img_hsv, lower_white, upper_white)
-    img_mask = cv2.inRange(img_hsv, lower_white, upper_white)
 
     # 원본 이미지를 가지고 Object 추출 이미지로 생성
     img_result = cv2.bitwise_and(img, img, mask=img_mask)
+
+    return img_result
+
+
+# 차선들의 이미지를 합치고 하늘배경을 제거하는 함수 #
+# img1, img2 인자는 합성할 이미지
+# pixel 은 이미지 위에서부터 제거할 픽셀수
+def merge_lines(img1, img2, pixel):
+    img_result = cv2.bitwise_or(img1, img2)
+
+    for i in range(pixel):
+        for j in range(160):
+            img_result[i, j] = [0, 0, 0]  # 검은색으로 채움
 
     return img_result
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-###################################################
-# 코드 작성일시 : 2020년 1월 11일
-# 코드 내용 : 전반적인 코드
-###################################################
+"""
+코드 작성일시 : 2020년 1월 11일
+작성자 : Park Jinsuk
+코드 내용 : 전반적인 코드
+"""
 
 import cv2
 import DataPreProcess as DPP
@@ -10,11 +11,13 @@ import DataPreProcess as DPP
 if __name__ == '__main__':
     print("this is main")
 
-    image = cv2.imread('test_image/1_cam-image_array_.jpg')  # 이미지 읽기
+    image = cv2.imread('test_image/2_cam-image_array_.jpg')  # 이미지 읽기
 
     a = DPP.find_line_orange(image)
     b = DPP.find_line_white(image)
+    c = DPP.merge_lines(a, b, 50)
 
-    cv2.imshow('result', a)
-    cv2.imshow('result', b)
+    cv2.imshow('a', a)
+    cv2.imshow('b', b)
+    cv2.imshow('c', c)
     cv2.waitKey(0)
