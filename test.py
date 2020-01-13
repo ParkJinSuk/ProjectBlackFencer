6f3071e4e4c25d54ee00e8c6c8e622b66460b09b
ParkJinSuk <ParkJinSuk@users.noreply.github.com>
2020-01-13 15:08:58 +0900

    filter

--- test.py
+++ test.py
@@ -2,19 +2,29 @@
 코드 작성일시 : 2020년 1월 11일
 코드 내용 : 각종 테스트를 위한 임시코드들
 """
-import os
+import cv2
+import DataPreProcess as DPP
 
-path = './test_image'
 
-for root, dirs, files in os.walk(path):
-    for fname in files:
-        full_fname = os.path.join(root, fname)
+cap = cv2.VideoCapture(0)
 
-        print(full_fname)
-        # print(len(full_fname))
+print('width :%d, height : %d' % (cap.get(3), cap.get(4)))
 
-print("other code")
+while(True):
+    ret, frame = cap.read()    # Read 결과와 frame
 
-file_list = os.listdir(path)
-print("file_list: {}".format(file_list))
-print("file_count: {}".format(len(file_list)))
+    if(ret) :
+        #image = cv2.cvtColor(frame,  cv2.COLOR_BGR2GRAY)    # 입력 받은 화면 Gray로 변환
+        image = cv2.GaussianBlur(frame, (5, 5), 0)
+        image = cv2.bilateralFilter(image, 15, 75, 75)
+        #result1 = DPP.filter_binary(image)
+        #result2 = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 5)
+        cv2.imshow('binary', image)    # 컬러 화면 출력
+        #cv2.imshow('binaryinv', result2)    # 컬러 화면 출력
+
+
+        #cv2.imshow('frame_gray', gray)    # Gray 화면 출력
+        if cv2.waitKey(1) == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
