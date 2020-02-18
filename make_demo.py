f6799b5a9bfa2095ba537f86d1fb822901a670a9
ParkJinSuk <ParkJinSuk@users.noreply.github.com>
2020-02-18 17:48:50 +0900

    .

--- make_demo.py
+++ make_demo.py
@@ -17,8 +17,12 @@ for i in range(len(file_list_read_frame)):
     frame = cv2.imread("demo_road/" + file_list_read_frame[i])  # 이미지 읽기
     frame_het = cv2.imread("demo_het/" + file_list_read_het[i])  # 이미지 읽기
 
-    # 이미지 전처리 과정
-    frame_display = dpp.filter_sobel(frame)
+    ##### 이미지 전처리 과정 ###########
+    _frame = dpp.filter_sobel(frame) # 차선검출
+
+    frame_het = cv2.GaussianBlur(frame_het, (5, 5), 0) # 온도이미지 가우시안블러
+    frame_display = cv2.add(_frame, frame_het)
+
 
     # 이미지 저장
     # cv2.imwrite("train_image/"+file_list_read[i], line_image)
@@ -27,7 +31,7 @@ for i in range(len(file_list_read_frame)):
     cv2.imshow("frame", frame)
     cv2.imshow('frame_het', frame_het)
     cv2.imshow('display', frame_display)
-    cv2.waitKey(10)
+    cv2.waitKey(0)
 
     # 변환 진행과정 표시
     print("{} of {}".format(i + 1, len(file_list_read_frame) + 1))
