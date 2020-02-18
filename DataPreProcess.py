d94ccf55b12d93b6f1d829be351efb3d6214f01b
ParkJinSuk <ParkJinSuk@users.noreply.github.com>
2020-02-18 22:36:27 +0900

    .

--- DataPreProcess.py
+++ DataPreProcess.py
@@ -71,3 +71,112 @@ def filter_sobel(img):
     img = cv2.bitwise_and(img, img, mask=mask)
 
     return img
+
+# yolo에서 받은 좌표와 물체크기를 이미지 합성
+def image_object(img, center_x, center_y, width, height):
+    center_x = int(center_x)
+    center_y = int(center_y)
+
+    width_half = int(width) / 2
+    height_half = int(height) / 2
+
+    # ROI 영역
+    vertices = np.array(
+        [[(center_x - width_half, center_y - height_half),
+          (center_x - width_half, center_y + height_half),
+          (center_x + width_half, center_y + height_half),
+          (center_x + width_half, center_y - height_half)]],
+        dtype=np.int32)
+    cv2.fillPoly(img, vertices, (255, 255, 0))
+
+    return img
+
+def yolo_arr2flat(yolo_location):
+    '''
+    yolo좌표(중심높이좌표, 중심너비좌표, 가로길이, 세로길이)
+    를 받아서 한줄로 flat하게 만들어 주는 함수
+    :param yolo_location: yolo로부터 받는 좌표
+    :return: flat yolo데이터
+    '''
+    x = yolo_location[0] # 위에서 아래로 중심 높이
+    y = yolo_location[1] # 왼쪽에서 오른쪽으로 중심 너비
+    w = yolo_location[2]
+    h = yolo_location[3]
+
+    width = 640
+    height = 480
+
+    flat_yolo = np.zeros((width*height), dtype=np.int8)
+
+    for i in range(10):
+        for j in range(10):
+            if (i+1 >= x-h/2) and (i+1 <= x+h/2):
+                if (j+1 >= y-w/2) and (j+1 <= y+w/2):
+                    flat_yolo[i*width + j] = 1
+
+    return flat_yolo
+
+def image_het2flat(image_het):
+    '''
+    카메라 이미지에 mapping이 된 온도이미지를
+    한줄로 flat하게 만들어주는 함수
+    :param image_het: mapping이 된 온도이미지
+    :return: flat 온도데이터
+    '''
+
+    width = 640
+    height = 480
+
+    img_het = cv2.resize(image_het, (width, height), interpolation=cv2.INTER_CUBIC)
+    result = np.zeros((width * height), dtype=np.int8)
+
+    for i in range(height):
+        for j in range(width):
+            if img_het[i, j] is (255, 100, 100):
+                result[i*width + j] = (1)
+            elif img_het[i, j] is (255, 200, 200):
+                result[i*width + j] = (1)
+            elif img_het[i, j] is (255, 255, 255):
+                result[i*width + j] = (1)
+            else:
+                result[i*width + j] = (0)
+
+    return result
+
+def Find_BlackIce(het, yolo):
+    '''
+    flat형식의 온도데이터(het), yolo좌표(yolo)의 데이터를
+    인덱스를 비교해가면서 둘다 1일때, 블랙아이스 리스트의
+    값을 0에서 1로 바꿔준다.
+    :param het: flat 온도데이터
+    :param yolo: flat yolo데이터
+    :return: flat 블랙아이스
+    '''
+
+    width = 640
+    height = 480
+
+    blackice = np.zeros((width*height), dtype=np.int8)
+    #print(het[4], yolo[4])
+    #print(het[4].dtype, yolo[4].dtype)
+    for index in range(len(het)):
+        if (het[index] is 1) and (yolo[index] is 1):
+            blackice[index] = 1
+            #print(blackice[index])
+    #print(blackice)
+    return blackice
+
+
+def image_blackice(list_blackice):
+    result = np.zeros(shape=(480, 640, 3), dtype="uint8")
+    width = 640
+    height = 480
+
+    for i in range(height):
+        for j in range(width):
+            if list_blackice[i*640+j] == 1:
+                result[i, j] = (255, 255, 255)
+            else:
+                result[i, j] = (0, 0, 0)
+
+    return result
--- make_demo.py
+++ make_demo.py
@@ -2,36 +2,62 @@ import cv2
 import os
 import numpy as np
 import DataPreProcess as dpp
+import csv
+import time
+
 
 path_read_frame = './demo_road'
 path_read_het = './demo_het'
-
 path_write = './demo_save'
+path_read_yolo = 'demo_yolo.csv'
 
 # 파일리스트 읽어오기
 file_list_read_frame = os.listdir(path_read_frame)
 file_list_read_het = os.listdir(path_read_het)
 file_list_write = os.listdir(path_write)
 
+with open('demo_yolo.csv', encoding='UTF8') as file:
+    csv_data = []
+    for line in file.readlines():
+        line = line.strip()
+        csv_data.append(line.split(','))
+
+print("yolo 시작프레임번호 : ", csv_data[0][0])
+print("yolo 끝프레임번호 : ", csv_data[96][0])
+csv_data = np.array(csv_data)
 for i in range(len(file_list_read_frame)):
+
     frame = cv2.imread("demo_road/" + file_list_read_frame[i])  # 이미지 읽기
     frame_het = cv2.imread("demo_het/" + file_list_read_het[i])  # 이미지 읽기
 
     ##### 이미지 전처리 과정 ###########
     _frame = dpp.filter_sobel(frame) # 차선검출
 
+
     frame_het = cv2.GaussianBlur(frame_het, (5, 5), 0) # 온도이미지 가우시안블러
     frame_display = cv2.add(_frame, frame_het)
 
+    # yolo 좌표값 발생히면 이미지 처리
+    for num in range(97):
+        if i == int(csv_data[num][0]):
+            yolo_loaction = [int(csv_data[num][1]), int(csv_data[num][2]), int(csv_data[num][3]), int(csv_data[num][4])]
+            list_yolo = dpp.yolo_arr2flat(yolo_loaction)
+            list_het = dpp.image_het2flat(frame_het)
+            list_blackice = dpp.Find_BlackIce(list_het, list_yolo)
+            frame_blackice = dpp.image_blackice(list_blackice)
+            cv2.imshow('blaic', frame_blackice)
+            frame_display = dpp.image_object(frame_display, csv_data[num][1], csv_data[num][2], csv_data[num][3], csv_data[num][4])
 
     # 이미지 저장
     # cv2.imwrite("train_image/"+file_list_read[i], line_image)
 
     # 변환 중인 이미지 보여줌
-    cv2.imshow("frame", frame)
-    cv2.imshow('frame_het', frame_het)
+    #cv2.imshow("frame", frame)
+    #cv2.imshow('frame_het', frame_het)
     cv2.imshow('display', frame_display)
-    cv2.waitKey(0)
+    cv2.waitKey(2)
 
     # 변환 진행과정 표시
-    print("{} of {}".format(i + 1, len(file_list_read_frame) + 1))
+    #print("{} of {}".format(i + 1, len(file_list_read_frame) + 1))
+
+print("######## end ##########")
