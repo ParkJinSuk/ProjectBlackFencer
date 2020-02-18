ad5923fe51d4570ad8c2f9f667bc1bc5b9d2581e
ParkJinSuk <ParkJinSuk@users.noreply.github.com>
2020-02-18 17:32:28 +0900

    .

--- make_demo.py
+++ make_demo.py
@@ -1,4 +1,33 @@
 import cv2
 import os
 import numpy as np
+import DataPreProcess as dpp
 
+path_read_frame = './demo_road'
+path_read_het = './demo_het'
+
+path_write = './demo_save'
+
+# 파일리스트 읽어오기
+file_list_read_frame = os.listdir(path_read_frame)
+file_list_read_het = os.listdir(path_read_het)
+file_list_write = os.listdir(path_write)
+
+for i in range(len(file_list_read_frame)):
+    frame = cv2.imread("demo_road/" + file_list_read_frame[i])  # 이미지 읽기
+    frame_het = cv2.imread("demo_het/" + file_list_read_het[i])  # 이미지 읽기
+
+    # 이미지 전처리 과정
+    frame_display = dpp.filter_sobel(frame)
+
+    # 이미지 저장
+    # cv2.imwrite("train_image/"+file_list_read[i], line_image)
+
+    # 변환 중인 이미지 보여줌
+    cv2.imshow("frame", frame)
+    cv2.imshow('frame_het', frame_het)
+    cv2.imshow('display', frame_display)
+    cv2.waitKey(10)
+
+    # 변환 진행과정 표시
+    print("{} of {}".format(i + 1, len(file_list_read_frame) + 1))
